9a033be26ee4c29133766091a425941e8a25d19f
Arie Knoester <arikstr@gmail.com>
2023-09-22 15:03:53 -0500

    Completed requirements.

--- main.py
+++ main.py
@@ -9,29 +9,31 @@ def initialize_screen():
     image = "blank_states_img.gif"
     new_screen.addshape(image)
     turtle.shape(image)
+    new_screen.setup(width=750, height=500)
     return new_screen
 
 
-state_locations = pandas.read_csv("50_states.csv")
+states = pandas.read_csv("50_states.csv")
 screen = initialize_screen()
-correct_guesses = []
+correct_states = []
+number_correct = 0
 text_display = TextDisplay()
 
-# TODO: refactor so that I'm not calling len(correct_guesses) multiple times.
-while len(correct_guesses) < 50:
+while number_correct < 50:
 
-    num_correct = len(correct_guesses)
-    user_input = turtle.textinput(title=f"{num_correct}/50 States Correct", prompt="Enter a state name.")
+    user_input = turtle.textinput(title=f"{number_correct}/50 States Correct", prompt="Enter a state name.")
     if user_input:
         user_input = user_input.title()
-        print(user_input)
     # User hits Cancel
     else:
         break
 
-    if user_input in state_locations["state"].values:
-        state_entered = state_locations[state_locations.state == user_input]
-        print(state_entered)
-
+    if user_input in states["state"].values:
+        state = states[states.state == user_input]
+        state_name = state.state.item()
+        if state_name not in correct_states:
+            correct_states.append(state_name)
+            number_correct = len(correct_states)
+            text_display.display_state_name(state_to_display=state)
 
 turtle.mainloop()
--- text_display.py
+++ text_display.py
@@ -5,7 +5,12 @@ class TextDisplay(Turtle):
 
     def __init__(self):
         super().__init__()
-        # self.hideturtle()
+        self.hideturtle()
+        self.penup()
 
-    def display_state_name(self):
-        pass
+    def display_state_name(self, state_to_display):
+        name = state_to_display.state.item()
+        xcor = float(state_to_display.x.item())
+        ycor = float(state_to_display.y.item())
+        self.goto((xcor, ycor))
+        self.write(name, align="center")
