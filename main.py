b15225226c160a7f05ed0e8eef791140f5eaf7ae
Arie Knoester <arikstr@gmail.com>
2023-10-02 13:10:45 -0500

    Updated comment.

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ while number_correct < 50:
             text_display.display_state_name(state_to_display=user_state)
 
 # If the player quits before guessing all 50 states, write the states missed to a file.
-# TODO: There's probably a less convoluted way of doing this.
+# TODO: There's probably a less convoluted way of doing this. This was just a first attempt to get it done.
 all_states_set = set(states["state"])
 correct_states_set = set(correct_states)
 states_missed = list(all_states_set - correct_states_set)
